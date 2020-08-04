f1eaec21c71a2e5a96d10e68a6898b1f004cf960
Jason Hinton <jason_hinton@att.net>
2020-08-03 21:50:25 -0400

    deleted projects folder

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const projectsRouter = require('./projects/projects-router.js')
+const db = require("../data/db-config.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use('/api/projects', projectsRouter)
+
+server.get("/api/projects", (req, res) => {
+	db('projects')
+		.then((project) => {
+			res.status(200).json(project);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "failed to retrieve project", err });
+		});
+});
+
+server.post("/api/projects", (req, res) => {
+	const newProject = req.body;
+	db("projects")
+		.insert(newProject)
+		.then((project) => {
+			res.status(201).json(project);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "failed to add project", err });
+		});
+});
+
 
 
 module.exports = server;
-
