1bb7c037409f9bebcb04762ee40569d3ed8ed312
Jason Hinton <jason_hinton@att.net>
2020-08-03 22:28:44 -0400

    mvp

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 
-const db = require("../data/db-config.js");
+const db = require("./data/db-config.js");
 
 const server = express();
 
@@ -9,7 +9,7 @@ server.use(helmet());
 server.use(express.json());
 
 server.get("/api/projects", (req, res) => {
-	db('projects')
+	db("projects")
 		.then((project) => {
 			res.status(200).json(project);
 		})
@@ -30,6 +30,56 @@ server.post("/api/projects", (req, res) => {
 		});
 });
 
+server.get("/api/resources", (req, res) => {
+	db("resources")
+		.then((resources) => {
+			res.status(200).json(resources);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "failed to load resources", err });
+		});
+});
 
+server.post("/api/resources", (req, res) => {
+	const newResource = req.body;
+	db("resources")
+		.insert(newResource)
+		.then((resource) => {
+			res.status(201).json(resource);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "failed to add resource", err });
+		});
+});
+server.get("/api/tasks", (req, res) => {
+	db("tasks as t")
+		.join("projects as p")
+		.select(
+			"p.name",
+			"p.description",
+			"t.task_description",
+			"t.notes",
+			"t.task_completed"
+		)
+		.orderBy("p.name")
+		.then((tasks) => {
+			res.status(200).json(tasks);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "failed to load tasks", err });
+		});
+});
+
+server.post("/api/tasks", (req, res) => {
+	const newTask = req.body;
+	db("tasks")
+		.insert(newTask)
+		.then((task) => {
+			res.status(201).json(task);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "failed to add task", err });
+		});
+});
 
 module.exports = server;
